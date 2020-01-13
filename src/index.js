748a43f2ba5f767b76455386c69560018e161366
Jimmy <bruno_pier@hotmail.com>
2020-01-13 14:56:29 -0300

    Reordering code in - index.js

--- src/index.js
+++ src/index.js
@@ -9,9 +9,9 @@ axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com';
 axios.defaults.headers.common['Authorization'] = 'AUTH TOKEN';
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 
-
 axios.interceptors.request.use(request => {
-    console.log(request)
+    console.log(request);
+    // Edit request config
     return request;
 }, error => {
     console.log(error);
@@ -19,7 +19,8 @@ axios.interceptors.request.use(request => {
 });
 
 axios.interceptors.response.use(response => {
-    console.log(response)
+    console.log(response);
+    // Edit request config
     return response;
 }, error => {
     console.log(error);
