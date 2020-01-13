55df26a2b92798c79e75790574745ecde5bb3d05
Jimmy <bruno_pier@hotmail.com>
2020-01-13 15:06:26 -0300

    Adding - Switching Between Pages - code - import NewPost - Blog.js

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -5,7 +5,6 @@ import { Route, Link } from 'react-router-dom';
 import './Blog.css';
 import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
-
 class Blog extends Component {
     render () {
         return (
@@ -25,6 +24,8 @@ class Blog extends Component {
                 {/* <Route path="/" exact render={() => <h1>Home</h1>} />
                 <Route path="/" render={() => <h1>Home 2</h1>} /> */}
                 <Route path="/" exact component={Posts} />
+                <Route path="/new-post" exact component={NewPost} />
+
             </div>
         );
     }
